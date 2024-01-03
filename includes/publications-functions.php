64e7d42a605e9d3b108bb95d9bf34f897bc1076d
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-01-03 17:50:16 +0000

    Add publication types, begin layout

---
+++ includes/publications-functions.php
@@ -0,0 +1,42 @@
+<?php
+// formats the publication types
+function pub_type($number) {
+    switch ($number) {
+        case 1:
+            return "Books";
+        case 2:
+            return "Book Chapters";
+        case 3:
+            return "Journal Papers";
+        case 4:
+            return "Conference Proceedings";
+        case 5:
+            return "Other Unreferenced Publications";
+        case 6:
+            return "Patents";
+        case 7:
+            return "Pending Patents";
+        case 8:
+            return "Disclosures";
+        case 9:
+            return "Theses or Dissertations";
+        case 10:
+            return "News Coverage";
+        case 11:
+            return "Presentations";
+        case 12:
+            return "Invited Presentations";
+        case 13:
+            return "Plenary Presentations";
+        case 14:
+            return "Tutorials";
+        case 15:
+            return "Posters";
+        case 16:
+            return "Workshops";
+        case 17:
+            return "Seminar";
+        default:
+            return "Invalid Type";
+    }
+}
---
+++ publications.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Publications
+Description: {{Short project description here}}
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: https://github.com/UCF/CREOL-Publications
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+
+
+require_once 'includes/publications-feed.php';
+require_once 'includes/publications-functions.php';
+require_once 'includes/publications-layout.php';
+require_once 'includes/publications-list.php';
+
+add_shortcode( 'publications', 'publications_list');
