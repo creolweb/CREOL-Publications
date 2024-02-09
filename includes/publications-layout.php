fe740a05cdfc102b1098c2257d544e361b31a2da
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-02-09 16:31:28 +0000

    .

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -84,7 +84,7 @@ function publications_form_display() {
 function publications_display( $year, $type, $author ) {
 	$url = 'https://api.creol.ucf.edu/PublicationsJson.asmx/PublicationInfo?Year=' . $_GET['year'] . '&Type=' . $_GET['type'] . '&Author=' . $_GET['author'];
 	$publication_info_arr = get_json_nocache( $url );
-	error_log($publication_info_arr);
+	error_log(json_encode($publication_info_arr));
 	?>
 	
 	<script>
