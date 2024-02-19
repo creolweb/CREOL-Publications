2ca9f8a8085f82250ce393834075cfea806cbe3b
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-02-19 19:38:21 +0000

    .

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -84,7 +84,7 @@ function publications_display($year, $type, $author, $page, $pageSize = 20) {
 	</script>
 	<?php
 	$totalPages = count($publication_info_arr);
-	$count = count($publication_info_arr->response);
+	// $count = count($publication_info_arr->response);
     // Display the current page and total number of pages
     echo "<div>Page $page of $totalPages</div>";
 
@@ -119,7 +119,7 @@ function publications_display($year, $type, $author, $page, $pageSize = 20) {
 	<div class="row float-right">
 		<!-- Found&nbsp;<span id="publicationCount"></span>&nbsp;publications. -->
 		Found <?= $totalPages ?> publications.
-		Found <?= $count ?> publications.
+		<!-- Found <?= $count ?> publications. -->
 	</div>
 	<br>
 	<script>
