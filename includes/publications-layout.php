fa3f37e68d528f6a3af92e3486220e1a8147c3e2
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-03-04 20:29:51 +0000

    ..

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -82,7 +82,7 @@
 				<?php
 				if ( isset( $_GET['yr'] ) && isset( $_GET['type'] ) && isset( $_GET['author'] ) ) {
 					if ( $_GET['yr'] == ALL_YEARS && $_GET['type'] == ALL_TYPES && $_GET['author'] == ALL_AUTHORS ) {
-						publications_display(ALL_YEARS, ALL_TYPES, ALL_AUTHORS, $_GET['pg'], "");
+						publications_display(ALL_YEARS, ALL_TYPES, ALL_AUTHORS, $_GET['pg'], $_GET['search']);
 					} else {
 
 						publications_display($_GET['yr'], $_GET['type'], $_GET['author'], $_GET['pg'], $_GET['search']);
