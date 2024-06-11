49878f09515032b60a88b445d47dbb3c7dd9b9f7
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-11 17:22:08 +0000

    ...

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -216,12 +216,12 @@ function publications_display( $year, $type, $author, $page, $search ) {
 	$range = 3;
 	echo '<div class="text-right">';
 	if ($page > 1) {		
-		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=1">First</a> ';
-		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . ($page - 1) . '"><i class="fa fa-caret-left" aria-hidden="true"></i></a> ';
+		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=1">First</a> ';
+		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . ($page - 1) . '"><i class="fa fa-caret-left" aria-hidden="true"></i></a> ';
 	}
 	else {
-		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=1">First</span> ';
-		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . ($page - 1) . '"><i class="fa fa-caret-left" aria-hidden="true"></i></span> ';
+		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=1">First</span> ';
+		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . ($page - 1) . '"><i class="fa fa-caret-left" aria-hidden="true"></i></span> ';
 	}
 
 	for ($x = ($page - $range); $x < (($page + $range) + 1); $x++) {
@@ -229,18 +229,18 @@ function publications_display( $year, $type, $author, $page, $search ) {
 			if ($x == $page) {
 				echo '<strong>' . $x . '</strong> ';
 			} else {
-				echo '<a href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . $x . '">' . $x .'</a> '; 
+				echo '<a href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . $x . '">' . $x .'</a> '; 
 			}
 		}
 	}
 
 	if ($page < $totalPages) {
-		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . ($page + 1) . '"><i class="fa fa-caret-right" aria-hidden="true"></i></a> ';
-		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . $totalPages . '">Last</a>';
+		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . ($page + 1) . '"><i class="fa fa-caret-right" aria-hidden="true"></i></a> ';
+		echo '<a href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . $totalPages . '">Last</a>';
 	}
 	else {
-		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . ($page + 1) . '"><i class="fa fa-caret-right" aria-hidden="true"></i></span> ';
-		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&author=' . $author . '&pg=' . $totalPages . '">Last</span>';
+		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . ($page + 1) . '"><i class="fa fa-caret-right" aria-hidden="true"></i></span> ';
+		echo '<span href="?pubyr=' . $year . '&type=' . $type . '&pubAuth=' . $author . '&pg=' . $totalPages . '">Last</span>';
 	}
 
 	echo '</div>';
