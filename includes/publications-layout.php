dc938225ebfcfd4aaf71249204693ce6782efa07
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-11 17:51:56 +0000

    .

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -47,7 +47,7 @@
 					</div>
 					<div class="col-xs-12 col-sm-6 col-md-2 form-group">
 						<select name="pubAuth" id="pubAuth" class="form-control" onchange="handleSelectorChange()" style="width: 100%;">
-							<option value="0">pubAuth</option>
+							<option value="0">Author</option>
 							<?php for ( $i = 0; $i < count( $pubAuth_arr ); $i++ ) : ?>
 								<option value="<?= $pubAuth_arr[ $i ]->PeopleID ?>">
 									<?= $pubAuth_arr[ $i ]->LastFirstName ?>
